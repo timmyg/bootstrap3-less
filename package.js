eececb908024d7fb8ba0323be39c96f55826cf00
Tim <timmyg13@gmail.com>
2014-04-22 15:32:15 -0400

    init

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+    summary: "Bootstrap 3, with Less files (v3.0.3)."
+}); 
+
+Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.use('less','client');
+    api.add_files('lib/js/bootstrap.js', 'client');
+
+    // fonts
+    api.add_files('lib/fonts/glyphicons-halflings-regular.eot', 'client');
+    api.add_files('lib/fonts/glyphicons-halflings-regular.ttf', 'client');
+    api.add_files('lib/fonts/glyphicons-halflings-regular.svg', 'client');
+    api.add_files('lib/fonts/glyphicons-halflings-regular.woff', 'client');
+});
